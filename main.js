b073469305c310a844203e953befe36b9b8c6a76
humbito <humberto.arosemena@gmail.com>
2023-09-05 19:52:29 -0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function(){
+    let boton = document.getElementById("boton")
+    let navbar = document.getElementById("navbar")
+    let parrafo = document.getElementById("parrafo")
+
+});
